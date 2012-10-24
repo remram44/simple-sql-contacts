c461975e77e64c630e1532012b030eee2a0ec894
Remi Rampin <remirampin@gmail.com>
2012-10-24 19:33:01 +0200

    Added some models.

--- contacts/models.py
+++ contacts/models.py
@@ -1,4 +1,38 @@
+from sqlalchemy import Column, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import Date, Integer, String
 
 
 Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = 'person'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    dateofbirth = Column(Date)
+
+    emails = relationship('Email', back_populates='person')
+    phones = relationship('PhoneNumber', back_populates='person')
+
+
+class Email(Base):
+    __tablename__ = 'email'
+
+    id = Column(Integer, primary_key=True) # useless, but the ORM needs it
+
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship('Person', uselist=False, back_populates='emails')
+    email = Column(String)
+
+
+class PhoneNumber(Base):
+    __tablename__ = 'phone'
+
+    id = Column(Integer, primary_key=True) # useless, but the ORM needs it
+
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship('Person', uselist=False, back_populates='phones')
+    phone = Column(String)
